8a3987111a0ed272b145135c6ff1cc50a1bcf161
Mark Roland <radiowhir@gmail.com>
2022-02-11 09:46:26 -0800

    Initial Jest Unit Test setup

---
+++ tests/PathHelper.test.js
@@ -0,0 +1,13 @@
+const PathHelper = require('./../src/PathHelper.js');
+
+const PH = new PathHelper();
+
+test('getMin', () => {
+  let path = [
+    [-2,  1],
+    [ 4,  1],
+    [ 4, -3],
+    [-2, -3]
+  ];
+  expect(PH.getMin(path)).toStrictEqual([-2, -3]);
+});
